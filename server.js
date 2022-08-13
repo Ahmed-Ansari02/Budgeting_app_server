2a1ccf096889b76cc6f0c0238f40c46c0b8e4767
Ahmed Ansari <ahmedansari@Ahmed-Notebook.attlocal.net>
2022-08-13 16:23:07 -0500

    readded cors library

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql2");
-
+const cors = require("cors")
 
 const app = express();
+const cors= app.use(cors())
 
 const PORT = process.env.PORT || 5000;
 const con = mysql.createConnection({
